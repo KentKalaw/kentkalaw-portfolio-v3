a7426b18ab2e0d566b2219e51e64210f5f293cd0
Kent Kalaw <kentfranciskalaw@gmail.com>
2026-01-27 12:07:51 +0800

    chore: updated about page

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -1,17 +1,26 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
+import { BriefcaseBusiness } from 'lucide-react';
 export default function About() {
   return (
-    <section className="mb-12">
-          <h2 className="text-2xl font-bold mb-4">About</h2>
-          <div className="prose dark:prose-invert max-w-none">
-            <p className="text-gray-700 dark:text-gray-300 mb-4">
-              I'm a full-stack software engineer specializing in developing modern web applications with JavaScript, TypeScript, and Python. 
-              I work on projects including building scalable web applications, mobile apps, and creating innovative solutions for businesses.
-            </p>
-            <p className="text-gray-700 dark:text-gray-300">
-              I'm passionate about learning new technologies and sharing knowledge with the developer community. 
-              Currently exploring AI integration and modern development workflows.
-            </p>
-          </div>
-        </section>
+    <section className="mb-3">
+    <Card className="">
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2 text-base md:text-xl font-bold">
+          <BriefcaseBusiness />
+          About
+          </CardTitle>
+      </CardHeader>
+      <CardContent className="prose text-sm md:text-sm dark:prose-invert max-w-none">
+        <p className="text-gray-900 dark:text-gray-500 mb-4">
+          I'm a Fresh Graduate Full-stack Software Developer with a passion in building web and mobile applications using modern technologies.
+          I work on projects including building scalable web applications, mobile apps, and creating innovative solutions for businesses.
+        </p>
+        <p className="text-gray-900 dark:text-gray-500">
+          I'm passionate about learning new technologies and sharing knowledge with the developer community. 
+          Currently exploring AI integration and modern development workflows.
+        </p>
+      </CardContent>
+    </Card>
+  </section>
   )
 }
