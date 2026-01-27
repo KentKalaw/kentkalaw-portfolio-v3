09b1f7ad3d86554a6ab8ee6bf4ec4854d4a3de08
Kent Kalaw <kentfranciskalaw@gmail.com>
2026-01-27 18:16:45 +0800

    chore: finished about page

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -5,17 +5,17 @@ export default function About() {
     <section className="mb-3">
     <Card className="">
       <CardHeader>
-        <CardTitle className="flex items-center gap-2 text-base md:text-xl font-bold">
+        <CardTitle className="font-mono flex items-center gap-2 text-base md:text-xl font-bold">
           <BriefcaseBusiness />
           About
           </CardTitle>
       </CardHeader>
       <CardContent className="prose text-sm md:text-sm dark:prose-invert max-w-none">
-        <p className="text-gray-900 dark:text-gray-500 mb-4">
+        <p className="text-gray-900 dark:text-gray-300 mb-4">
           I'm a Fresh Graduate Full-stack Software Developer with a passion in building web and mobile applications using modern technologies.
-          I work on projects including building scalable web applications, mobile apps, and creating innovative solutions for businesses.
+          I work on projects including building web applications, mobile applications, and RESTful APIs.
         </p>
-        <p className="text-gray-900 dark:text-gray-500">
+        <p className="text-gray-900 dark:text-gray-300">
           I'm passionate about learning new technologies and sharing knowledge with the developer community. 
           Currently exploring AI integration and modern development workflows.
         </p>
