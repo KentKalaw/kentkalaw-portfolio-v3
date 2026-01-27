9467bdaa948902fd83f9d03e1997308c23608b03
Kent Kalaw <kentfranciskalaw@gmail.com>
2026-01-27 18:19:18 +0800

    feat: added github contri graph

--- src/components/pages/github-contribution.tsx
+++ src/components/pages/github-contribution.tsx
@@ -2,23 +2,25 @@ import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
 import { ChartLine } from "lucide-react"
 
 export default function GithubContributionsCard() {
-  return (
-    <Card className="mb-12">
-      <CardHeader>
-        <CardTitle className="flex items-center gap-2 text-base md:text-xl font-bold">
-            <ChartLine />
-            GitHub Contributions</CardTitle>
-      </CardHeader>
-      <CardContent className="flex flex-col items-center">
-        <img
-          src="https://ghchart.rshah.org/kentkalaw"
-          alt="KentKalaw's GitHub contributions chart"
-          className="w-full max-w-xs md:max-w-xl rounded-lg"
-        />
-        <p className="mt-3 text-sm text-gray-600 dark:text-gray-400 text-center">
-          My GitHub contributions heatmap. Updated in real-time.
-        </p>
-      </CardContent>
-    </Card>
-  )
+    return (
+        <section className="mb-3">
+            <Card>
+                <CardHeader>
+                    <CardTitle className="font-mono flex items-center gap-2 text-base md:text-xl font-bold">
+                        <ChartLine />
+                        Public GitHub Contributions</CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col items-center">
+                    <img
+                        src="https://ghchart.rshah.org/kentkalaw"
+                        alt="KentKalaw's GitHub contributions chart"
+                        className="w-full max-w-xs md:max-w-xl rounded-lg"
+                    />
+                    <p className="mt-3 text-sm text-gray-600 dark:text-gray-400 text-center">
+                        My Public GitHub contributions heatmap. Updated in real-time. Private contributions are not shown.
+                    </p>
+                </CardContent>
+            </Card>
+        </section>
+    )
 }
