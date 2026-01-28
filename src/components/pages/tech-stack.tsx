c121c374ead84ddb2d6d8e1ce5c9d46a9b787d3f
Kent Kalaw <kentfranciskalaw@gmail.com>
2026-01-29 00:44:28 +0800

    chore: changed from infinite slider to carousel

--- src/components/pages/tech-stack.tsx
+++ src/components/pages/tech-stack.tsx
@@ -1,89 +1,76 @@
 "use client"
 
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
-import { Type, TypeOutline } from "lucide-react"
-import { useState, useRef, useEffect } from "react"
-import { InfiniteSlider } from "@/components/ui/infinite-slider";
-import { ProgressiveBlur } from "@/components/ui/progressive-blur";
-
+import { Braces } from "lucide-react"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
 export default function TechStack() {
-  const techStackData = [
-    {
-      category: "Frontend",
-      skills: ["JavaScript", "TypeScript", "React", "Next.js", "Tailwind CSS"],
-    },
-    {
-      category: "Backend",
-      skills: ["Node.js", "PHP", "Laravel", "PostgreSQL", "MySQL"],
-    },
-    {
-      category: "DevOps & Cloud",
-      skills: ["AWS", "Docker", "Supabase"],
-    },
-    
-  ]
 
-  const [showAll, setShowAll] = useState(false)
-  const extraRef = useRef<HTMLDivElement>(null)
-  const [height, setHeight] = useState("0px")
-
-  useEffect(() => {
-    if (extraRef.current) {
-      setHeight(showAll ? `${extraRef.current.scrollHeight}px` : "0px")
-    }
-  }, [showAll])
-
-  const mainStack = techStackData.slice(0, 2)
-  const extraStack = techStackData.slice(2)
+  const techIcons = [
+    "html5/html5-original.svg",
+    "css3/css3-original.svg",
+    "javascript/javascript-original.svg",
+    "react/react-original.svg",
+    "nodejs/nodejs-original.svg",
+    "php/php-original.svg",
+    "nextjs/nextjs-original.svg",
+    "bootstrap/bootstrap-original.svg",
+    "tailwindcss/tailwindcss-original.svg",
+    "mysql/mysql-original-wordmark.svg",
+    "laravel/laravel-original.svg",
+    "postgresql/postgresql-original.svg",
+    "supabase/supabase-original.svg"
+  ];
 
   return (
     <section className="mb-3 animate-fade-in animate-delay-200">
-      <Card className="mt-3">
-  <CardHeader>
-    <CardTitle className="font-mono flex items-center gap-2 text-base md:text-xl font-bold"><TypeOutline />
-      Tech Stack
-    </CardTitle>
-  </CardHeader>
+      <Card>
+        <CardHeader>
+          <CardTitle className="font-mono flex items-center gap-2 text-base md:text-xl font-bold">
+            <Braces />
+            Tech Stack
+          </CardTitle>
+        </CardHeader>
 
-  <CardContent className="relative overflow-hidden py-6 will-change-transform">
-    <InfiniteSlider speedOnHover={30} speed={50} gap={40}>
-      {[
-        "html5/html5-original.svg",
-        "css3/css3-original.svg",
-        "javascript/javascript-original.svg",
-        "react/react-original.svg",
-        "nodejs/nodejs-original.svg",
-        "php/php-original.svg",
-        "nextjs/nextjs-original.svg",
-        "bootstrap/bootstrap-original.svg",
-        "tailwindcss/tailwindcss-original.svg",
-        "mysql/mysql-original-wordmark.svg",
-      ].map((icon, index) => (
-        <div key={index} className="flex-shrink-0 flex justify-center">
-          <img
-            className="h-10 w-auto mx-6"
-            src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${icon}`}
-            alt={icon.split("/")[0] + " logo"}
-          />
-        </div>
-      ))}
-    </InfiniteSlider>
-
-    <div className="pointer-events-none absolute inset-y-0 left-0 w-16 bg-gradient-to-r from-background" />
-    <div className="pointer-events-none absolute inset-y-0 right-0 w-16 bg-gradient-to-l from-background" />
-
-    <ProgressiveBlur
-      className="pointer-events-none absolute left-0 top-0 h-full w-16 will-change-transform"
-      direction="left"
-      blurIntensity={0.5}
-    />
-    <ProgressiveBlur
-      className="pointer-events-none absolute right-0 top-0 h-full w-16 will-change-transform"
-      direction="right"
-      blurIntensity={0.5}
-    />
-  </CardContent>
-</Card>
+        <CardContent className="relative overflow-hidden py-6 flex items-center justify-center">
+          <Carousel
+            className="w-full max-w-2xl"
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            plugins={[
+              Autoplay({
+                delay: 1250,
+                stopOnInteraction: false,
+                stopOnMouseEnter: true,
+              }),
+            ]}
+          >
+            <CarouselContent>
+              {techIcons.map((icon, index) => (
+                <CarouselItem className="basis-1/5" key={index}>
+                  <div className="flex justify-center">
+                    <img
+                      className="h-10 w-auto mx-6"
+                      src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${icon}`}
+                      alt={icon.split("/")[0] + " logo"}
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </CardContent>
+      </Card>
     </section>
   )
 }
