525320f300cb7854e5a6012b7b79e8774868d600
Kent Kalaw <kentfranciskalaw@gmail.com>
2026-01-29 00:43:32 +0800

    chore: updated front page ui

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -11,11 +11,11 @@ export default function About() {
           </CardTitle>
       </CardHeader>
       <CardContent className="prose text-sm md:text-sm dark:prose-invert max-w-none">
-        <p className="text-gray-900 dark:text-gray-300 mb-4">
+        <p className="text-gray-900 dark:text-gray-300 mb-2">
           I'm a Fresh Graduate Full-stack Software Developer with a passion in building web and mobile applications using modern technologies.
           I work on projects including building web applications, mobile applications, and RESTful APIs.
         </p>
-        <p className="text-gray-900 dark:text-gray-300">
+        <p className="text-gray-900 dark:text-gray-300 mb-2">
           I'm passionate about learning new technologies and sharing knowledge with the developer community. 
           Currently exploring AI integration and modern development workflows.
         </p>
