095cb9acec85827750e574c7bc36acfa2e9e944d
Kent Kalaw <kentfranciskalaw@gmail.com>
2026-01-30 22:58:11 +0800

    chore: added tooltip for tech stack

--- src/components/pages/tech-stack.tsx
+++ src/components/pages/tech-stack.tsx
@@ -9,22 +9,28 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import Autoplay from "embla-carousel-autoplay";
 export default function TechStack() {
   const techIcons = [
-    "html5/html5-original.svg",
-    "css3/css3-original.svg",
-    "javascript/javascript-original.svg",
-    "react/react-original.svg",
-    "nodejs/nodejs-original.svg",
-    "php/php-original.svg",
-    "nextjs/nextjs-original.svg",
-    "bootstrap/bootstrap-original.svg",
-    "tailwindcss/tailwindcss-original.svg",
-    "mysql/mysql-original-wordmark.svg",
-    "laravel/laravel-original.svg",
-    "postgresql/postgresql-original.svg",
-    "supabase/supabase-original.svg",
+    { path: "html5/html5-original.svg", name: "HTML5" },
+    { path: "css3/css3-original.svg", name: "CSS3" },
+    { path: "javascript/javascript-original.svg", name: "JavaScript" },
+    { path: "react/react-original.svg", name: "React" },
+    { path: "nodejs/nodejs-original.svg", name: "Node.js" },
+    { path: "php/php-original.svg", name: "PHP" },
+    { path: "nextjs/nextjs-original.svg", name: "Next.js" },
+    { path: "bootstrap/bootstrap-original.svg", name: "Bootstrap" },
+    { path: "tailwindcss/tailwindcss-original.svg", name: "Tailwind CSS" },
+    { path: "mysql/mysql-original-wordmark.svg", name: "MySQL" },
+    { path: "laravel/laravel-original.svg", name: "Laravel" },
+    { path: "postgresql/postgresql-original.svg", name: "PostgreSQL" },
+    { path: "supabase/supabase-original.svg", name: "Supabase" },
   ];
 
   return (
@@ -38,36 +44,45 @@ export default function TechStack() {
         </CardHeader>
 
         <CardContent className="relative flex items-center justify-center overflow-hidden py-6">
-          <Carousel
-            className="w-full max-w-2xl"
-            opts={{
-              align: "start",
-              loop: true,
-            }}
-            plugins={[
-              Autoplay({
-                delay: 1250,
-                stopOnInteraction: false,
-                stopOnMouseEnter: true,
-              }),
-            ]}
-          >
-            <CarouselContent>
-              {techIcons.map((icon, index) => (
-                <CarouselItem className="basis-1/3 md:basis-1/5" key={index}>
-                  <div className="flex justify-center">
-                    <img
-                      className="mx-6 h-10 w-auto"
-                      src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${icon}`}
-                      alt={icon.split("/")[0] + " logo"}
-                    />
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious className="left-0 md:-left-12" />
-            <CarouselNext className="right-0 md:-right-12" />
-          </Carousel>
+          <TooltipProvider>
+            <Carousel
+              className="w-full max-w-2xl"
+              opts={{
+                align: "start",
+                loop: true,
+              }}
+              plugins={[
+                Autoplay({
+                  delay: 1250,
+                  stopOnInteraction: false,
+                  stopOnMouseEnter: true,
+                }),
+              ]}
+            >
+              <CarouselContent>
+                {techIcons.map((icon, index) => (
+                  <CarouselItem className="basis-1/3 md:basis-1/5" key={index}>
+                    <div className="flex justify-center">
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <img
+                            className="mx-6 h-10 w-auto cursor-pointer"
+                            src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${icon.path}`}
+                            alt={icon.name + " logo"}
+                          />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>{icon.name}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious className="left-0 md:-left-12" />
+              <CarouselNext className="right-0 md:-right-12" />
+            </Carousel>
+          </TooltipProvider>
         </CardContent>
       </Card>
     </section>
