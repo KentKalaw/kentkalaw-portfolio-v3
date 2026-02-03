5deacfced2d1fb1eb06aae7d776cd6588917b169
Kent Kalaw <kentfranciskalaw@gmail.com>
2026-02-03 14:27:27 +0800

    chore: changed email to dynamic

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,6 +3,9 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
+
+  const myEmail = "kentfranciskalaw@gmail.com";
+  
   try {
     const { name, email, subject, message } = await req.json();
 
@@ -15,7 +18,7 @@ export async function POST(req: Request) {
 
     const data = await resend.emails.send({
       from: "Portfolio Contact <onboarding@resend.dev>", 
-      to: "kentfranciskalaw@gmail.com",
+      to: myEmail,
       replyTo: email,
       subject: `New Message: ${subject}`,
       html: `
